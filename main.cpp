e15c79ab21545f97249aed1a2a3a38d50c518df1
hoogMEH <108665410+yasharkind@users.noreply.github.com>
2024-08-21 16:01:41 -0700

    cleanup + comments + some cool animations and incPoints()

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 
 #include "renderer.h"
 
-point3D rotateX(point3D p, float deg) {
-    // 3D point rotation matrices
+point3D rotateX(point3D p, float deg)
+{
+    // 3D point rotation matrices over X
     float x = p.x;
     float y = p.y;
     float z = p.z;
-    point3D res = {x, (y * cosf(deg) - ( z * sinf(deg))), (y * sinf(deg) + z * cosf(deg))};
+    point3D res = {x, (y * cosf(deg) - (z * sinf(deg))), (y * sinf(deg) + z * cosf(deg))};
     return res;
 }
 
-point3D rotateY(point3D p, float deg) {
-    // 3D point rotation matrices
+point3D rotateY(point3D p, float deg)
+{
+    // 3D point rotation matrices over Y
     float x = p.x;
     float y = p.y;
     float z = p.z;
@@ -21,8 +23,9 @@ point3D rotateY(point3D p, float deg) {
     return res;
 }
 
-point3D rotateZ(point3D p, float deg) {
-    // 3D point rotation matrices
+point3D rotateZ(point3D p, float deg)
+{
+    // 3D point rotation matrices over Z
     float x = p.x;
     float y = p.y;
     float z = p.z;
@@ -30,47 +33,114 @@ point3D rotateZ(point3D p, float deg) {
     return res;
 }
 
-int main() {
-    Renderer* renderer = new Renderer(150, 50);
-    point3D fur = { 0.5,  0.5, -.5};
-    point3D ful = {-0.5,  0.5, -.5};
-    point3D fdr = { 0.5, -0.5, -.5};
-    point3D fdl = {-0.5, -0.5, -.5};
-    point3D bur = { 0.5,  0.5,  .5};
-    point3D bul = {-0.5,  0.5,  .5};
-    point3D bdr = { 0.5, -0.5,  .5};
-    point3D bdl = {-0.5, -0.5,  .5};
+point3D incPoint(point3D p, float val)
+{
     
-    float ro = 0.01;
-    while(true) {
-        fur = rotateX(rotateY(rotateZ(fur, ro), ro), ro);
-        ful = rotateX(rotateY(rotateZ(ful, ro), ro), ro);
-        fdr = rotateX(rotateY(rotateZ(fdr, ro), ro), ro);
-        fdl = rotateX(rotateY(rotateZ(fdl, ro), ro), ro);
-        bur = rotateX(rotateY(rotateZ(bur, ro), ro), ro);
-        bul = rotateX(rotateY(rotateZ(bul, ro), ro), ro);
-        bdr = rotateX(rotateY(rotateZ(bdr, ro), ro), ro);
-        bdl = rotateX(rotateY(rotateZ(bdl, ro), ro), ro);
-        ro += 0.01;
-        renderer->Line3D(&ful, &fur);
-        renderer->Line3D(&fur, &fdr);
-        renderer->Line3D(&fdr, &fdl);
-        renderer->Line3D(&fdl, &ful);
-        renderer->Line3D(&fur, &bur);
-        renderer->Line3D(&ful, &bul);
-        renderer->Line3D(&fdr, &bdr);
-        renderer->Line3D(&fdl, &bdl);
-        renderer->Line3D(&bul, &bur);
-        renderer->Line3D(&bur, &bdr);
-        renderer->Line3D(&bdr, &bdl);
-        renderer->Line3D(&bdl, &bul);
+    // move a point along all axis by +val if positive or -val if negative
+    
 
+    if (p.x > 0.0)
+    {
+        p.x = .5 + val;
+    }
+    else if (p.x < 0.0)
+    {
+        p.x = -.5 - val;
+    }
 
-        system("cls");
-        std::cout << renderer->Render() << std::endl;
-        renderer->Clear();
+    if (p.y > 0.0)
+    {
+        p.y = .5 + val;
+    }
+    else if (p.y < 0.0)
+    {
+        p.y = -.5 - val;
+    }
+
+    if (p.z > 0.0)
+    {
+        p.z = .5 + val;
+    }
+    else if (p.z < 0.0)
+    {
+        p.z = -.5 - val;
+    }
+
+    return p;
+}
+
+int main()
+{
+    Renderer *renderer = new Renderer(150, 50);
+
+    point3D cube1[] = {      
+        { 0.25,  0.25, -.25},  // fur 0
+        {-0.25,  0.25, -.25},  // ful 1
+        { 0.25, -0.25, -.25},  // fdr 2
+        {-0.25, -0.25, -.25},  // fdl 3
+        { 0.25,  0.25,  .25},  // bur 4
+        {-0.25,  0.25,  .25},  // bul 5
+        { 0.25, -0.25,  .25},  // bdr 6
+        {-0.25, -0.25,  .25},  // bdl 7
+    };
+
+    int EBO[] = {
+        // tells which index of points should be connected together
+        0, 1,
+        1, 3,
+        3, 2,
+        0, 2,
+        0, 4,
+        1, 5,
+        2, 6,
+        3, 7,
+        4, 5,
+        5, 7,
+        7, 6, 
+        6, 4,
+    };
+    point3D cube2[] = {
+        { 0.75,  0.75, -.75}, // fur 0
+        {-0.75,  0.75, -.75}, // ful 1
+        { 0.75, -0.75, -.75}, // fdr 2
+        {-0.75, -0.75, -.75}, // fdl 3
+        { 0.75,  0.75,  .75}, // bur 4
+        {-0.75,  0.75,  .75}, // bul 5
+        { 0.75, -0.75,  .75}, // bdr 6
+        {-0.75, -0.75,  .75}, // bdl 7 
+    };
+
+    float frame = 0;
+    float ro;
+    while (true)
+    {
+        frame++;
+        ro = sinf(frame / 10) * .3;
+
+        // rotate and move
+        for (int i = 0; i < 8; i++) {
+            // rotate and move each point to make an animation
+            // rotates by a value between -.3 and .3
+            // moves between -75 and -25 or 25 and 75 for x, y and z
+            cube1[i] = rotateX(rotateY(rotateZ(incPoint(cube1[i],  sin(frame / 10) / 4), ro), ro), ro); 
+            cube2[i] = rotateX(rotateY(rotateZ(incPoint(cube2[i], -sin(frame / 10) / 4), ro), ro), ro); 
+        }
+
+        for (int i = 0; i < 24; i+=2) {
+            // draw each line by connecting each vertex index of each cube from EBO 
+            renderer->Line3D(&cube1[EBO[i]], &cube1[EBO[i+1]]);
+            renderer->Line3D(&cube2[EBO[i]], &cube2[EBO[i+1]]);
+        }
+
+        // connect both cubes
+        for (int i = 0; i < 8; i++){
+            renderer->Line3D(&cube1[i], &cube2[i]);
+        }
+
+        system("cls"); // clear console system("clear"); for linux
+        std::cout << renderer->Render() << std::endl; // print the rendered string
+        renderer->Clear(); // clear the buffer
     }
 
     return 0;
-
 }
